2a3dd44e91393c9f581463c7aff8263c6c5a017b
Jorbbey <jorbbey@gmailcom>
2024-12-20 07:45:11 +0100

    commit 6

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,8 @@ const nextConfig = {
   env: {
     API_URL: process.env.API_URL,
   },
+  // Add this section to ensure Next.js functions as a serverless app on Netlify
+  target: "serverless",
 };
 
 export default nextConfig;
